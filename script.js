17545e7fb2b7147d493e8599c874e7c47044130c
Ess <afawen.com>
2025-03-23 12:39:53 +0100

    git@github.com:Awen1993/demo.git

--- script.js
+++ script.js
@@ -63,4 +63,19 @@ function payNow(amount) {
     } else {
         alert("Invalid Payment Option. Please try again.");
     }
-}
+}//Sliding
+var swiper = new Swiper(".swiper", {  // Target the correct container
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});

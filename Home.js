531d66f23654fbf5b5c0c106429c2a7615a6afbf
Ess <afawen.com>
2025-03-25 18:49:04 +0100

    git@github.com:Awen1993/demo.git

---
+++ Home.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelectorAll(".hero-slide");
+    const slider = document.querySelector(".hero-slider");
+    let index = 0;
+
+    function moveSlide() {
+        index++;
+        if (index >= slides.length) index = 0;
+        slider.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    setInterval(moveSlide, 4000); // Slide every 4 seconds
+});
